6f29c96ca0cc2e970b398fb10781c6ca5f4e8a4f
johnloan <75475273+johnloan@users.noreply.github.com>
2025-10-27 22:54:09 +0100

    Delete routes directory

--- routes/token/rtm.dart
+++
@@ -1,6 +0,0 @@
-import 'package:dart_frog/dart_frog.dart';
-import '../token_common.dart';
-
-Future<Response> onRequest(RequestContext context) async {
-  return handleTokenRequest(context, type: 'rtm');
-}
