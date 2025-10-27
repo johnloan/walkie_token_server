145d2cc495d4b7805949cdd3c62c11454b07214a
johnloan <75475273+johnloan@users.noreply.github.com>
2025-10-27 23:12:03 +0100

    Delete api/index.js

--- api/index.js
+++
@@ -1,5 +0,0 @@
-// TODO: This file will forward requests to Dart Frog build.
-// Next step: After Dart Frog build is provided, this file will require the compiled handler.
-module.exports = (req, res) => {
-  res.status(501).json({ error: "Build step required. Contact support to finalize adapter." });
-};
